60ea508917ffbd2b22a2aed4ef2727bcf0995767
SpoorthiRaj <spoorthitaj81@gmail.com>
2026-03-20 11:16:06 +0530

    force cors manual fix

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-app.options("*", cors());
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
